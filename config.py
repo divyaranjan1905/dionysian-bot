70e9a4b12b3b3fb6b680073058a4b92b20c405b1
divyaranjan1905 <69352099+divyaranjan1905@users.noreply.github.com>
2021-05-23 16:08:13 +0530

    change of token

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 api_id = "1793357"
 api_hash ="2a575f54c7e56a19c007b220d861d333"
-bot_token = "1520740480:AAFmzNr57pNnWud7u5x7sFX0MDPi2kPA6Dw"
+bot_token = "1520740480:AAHYa_UOxNw3RjbweFDBJU-UKPz7D1U41iA"
 pw = "divya6248"
 
 
